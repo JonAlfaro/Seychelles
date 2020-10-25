cb047a01648f37aee0d4e3e219a9712090dbccfb
Michael Burnett <michael23burnett@gmail.com>
2020-10-25 15:04:38 +1000

    Added scene navigation and exit button

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void ChangeSceneTo(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
