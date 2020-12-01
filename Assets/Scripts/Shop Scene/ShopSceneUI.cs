bf3f181325ad2631886e06596ab0e350b44c2539
Michael Burnett <michael23burnett@gmail.com>
2020-12-01 18:31:24 +1000

    Added shop scene UI and added game data manager to each scene
    The game data manger is a dont destroy on load and single instance anyway but its easier if its on each scene so that we can play from that scene without any data issues

---
+++ Assets/Scripts/Shop Scene/ShopSceneUI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+public class ShopSceneUI : MonoBehaviour
+{
+    public Text PremiumCurrencyText;
+
+    private void Awake()
+    {
+        Assert.IsNotNull(PremiumCurrencyText);
+    }
+
+    private void Start()
+    {
+        PremiumCurrencyText.text = $"{Constants.PremiumCurrencyName}: {GameDataManager.Instance.GameData.PremiumCurrency}";
+    }
+}
