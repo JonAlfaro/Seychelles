8176db45e487b02789d5567b49f85c792abc6c1c
JonAlfaro <j_alfa_4@outlook.com>
2020-12-25 20:41:45 +1000

    updated project

---
+++ Assets/Scripts/DEBUG/csDebug.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+
+public class csDebug : MonoBehaviour
+{
+    public MobManager mobManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            var mobIndex = mobManager.currentMobs.Count;
+            mobManager.AttackMob(mobIndex-1, 1);
+        }
+    }
+    
+    
+}
