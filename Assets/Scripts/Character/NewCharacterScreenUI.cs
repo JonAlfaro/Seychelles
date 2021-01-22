32e893379717ec2c77c79c9c7c0ed5f18ccf494c
Michael Burnett <michael23burnett@gmail.com>
2021-01-22 18:36:41 +1000

    Added 10x rolling

--- Assets/Scripts/Character/NewCharacterScreenUI.cs
+++ Assets/Scripts/Character/NewCharacterScreenUI.cs
@@ -6,17 +6,25 @@ using UnityEngine.UI;
 public class NewCharacterScreenUI : MonoBehaviour
 {
     public Image CharacterImage;
+    public Image[] MultiCharacterImages;
     public Text CharacterNameText;
     private CharacterData NewCharacter;
 
     private void Awake()
     {
         Assert.IsNotNull(CharacterImage);
+        Assert.IsTrue(MultiCharacterImages.Length == 10);
         Assert.IsNotNull(CharacterNameText);
     }
 
     public void SetCharacter(CharacterData character)
     {
+        CharacterImage.gameObject.SetActive(true);
+        foreach (Image image in MultiCharacterImages)
+        {
+            image.gameObject.SetActive(false);
+        }
+        
         string imageResourceName = $"{character.Id.ToString()}{Constants.BremiumResourceSuffix}";
         // TODO Resources.Load should be cached for performance. Ignoring it for now since it's not important on this project
         Sprite characterSprite =
@@ -24,5 +32,22 @@ public class NewCharacterScreenUI : MonoBehaviour
         CharacterImage.preserveAspect = true;
         CharacterImage.sprite = characterSprite;
         CharacterNameText.text = $"{character.Name} unlocked! Poggies!";
+    }
+
+    public void SetCharacters(CharacterData[] characters)
+    {
+        CharacterImage.gameObject.SetActive(false);
+        for (int i = 0; i < MultiCharacterImages.Length; i++)
+        {
+            MultiCharacterImages[i].gameObject.SetActive(true);
+            
+            string imageResourceName = $"{characters[i].Id.ToString()}{Constants.BremiumResourceSuffix}";
+            // TODO Resources.Load should be cached for performance. Ignoring it for now since it's not important on this project
+            Sprite characterSprite =
+                Resources.Load<Sprite>(Path.Combine(Constants.CharacterResourceFolder, imageResourceName));
+            MultiCharacterImages[i].preserveAspect = true;
+            MultiCharacterImages[i].sprite = characterSprite;
+        }
+        
     }
 }
--- Assets/Scripts/Shop Scene/ShopSceneUI.cs
+++ Assets/Scripts/Shop Scene/ShopSceneUI.cs
@@ -8,8 +8,10 @@ public class ShopSceneUI : MonoBehaviour
 {
     public Text PremiumCurrencyText;
     public Text RollCostText;
+    public Text TenRollCostText;
     public Text TotalUnlockedCharactersText;
     public Button RollButton;
+    public Button TenRollButton;
     public NewCharacterScreenUI NewCharacterScreen;
 
     private void Awake()
@@ -17,7 +19,9 @@ public class ShopSceneUI : MonoBehaviour
         Assert.IsNotNull(PremiumCurrencyText);
         Assert.IsNotNull(TotalUnlockedCharactersText);
         Assert.IsNotNull(RollCostText);
+        Assert.IsNotNull(TenRollCostText);
         Assert.IsNotNull(RollButton);
+        Assert.IsNotNull(TenRollButton);
         Assert.IsNotNull(NewCharacterScreen);
     }
 
@@ -45,6 +49,35 @@ public class ShopSceneUI : MonoBehaviour
             ShowNewCharacterScreen(unlockedCharacter);
         }
     }
+    
+    public void Roll10()
+    {
+        if (GameDataManager.Instance.GameData.PremiumCurrency >= Constants.GachaCost * 10)
+        {
+            GameDataManager.Instance.GameData.PremiumCurrency -= Constants.GachaCost * 10;
+            CharacterData[] unlockedCharacters =
+            {
+                CharacterList.GetRandomCharacter(),
+                CharacterList.GetRandomCharacter(),
+                CharacterList.GetRandomCharacter(),
+                CharacterList.GetRandomCharacter(),
+                CharacterList.GetRandomCharacter(),
+                CharacterList.GetRandomCharacter(),
+                CharacterList.GetRandomCharacter(),
+                CharacterList.GetRandomCharacter(),
+                CharacterList.GetRandomCharacter(),
+                CharacterList.GetRandomCharacter(),
+            };
+            
+            foreach (CharacterData character in unlockedCharacters)
+            {
+                GameDataManager.Instance.AddUnlockedCharacter(character);
+            }
+            GameDataManager.Instance.Save();
+            SetUpUI();
+            ShowNewCharactersScreen(unlockedCharacters);
+        }
+    }
 
     public void FreeMoneyForTesting()
     {
@@ -63,13 +96,21 @@ public class ShopSceneUI : MonoBehaviour
         NewCharacterScreen.SetCharacter(character);
         NewCharacterScreen.gameObject.SetActive(true);
     }
+    
+    private void ShowNewCharactersScreen(CharacterData[] characters)
+    {
+        NewCharacterScreen.SetCharacters(characters);
+        NewCharacterScreen.gameObject.SetActive(true);
+    }
 
     private void SetUpUI()
     {
         RollButton.interactable = GameDataManager.Instance.GameData.PremiumCurrency >= Constants.GachaCost;
+        TenRollButton.interactable = GameDataManager.Instance.GameData.PremiumCurrency >= Constants.GachaCost * 10;
         PremiumCurrencyText.text =
             $"{GameDataManager.Instance.GameData.PremiumCurrency} {Constants.PremiumCurrencyName}";
         RollCostText.text = $"${Constants.GachaCost}";
+        TenRollCostText.text = $"${Constants.GachaCost * 10}";
         
         int unlockedCharacterCount = GameDataManager.Instance.GameData.UnlockedCharacters.Length +
                                      GameDataManager.Instance.GameData.SelectedCharacters.Count(character => character != null);
